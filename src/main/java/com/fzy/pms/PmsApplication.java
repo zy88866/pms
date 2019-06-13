034f3038e25e0b30670d9692f35940a067ed0316
fzy <1015272918@qq.com>
2019-06-13 17:35:39 +0800

    添加任务调度

--- src/main/java/com/fzy/pms/PmsApplication.java
+++ src/main/java/com/fzy/pms/PmsApplication.java
@@ -1,8 +1,6 @@
 package com.fzy.pms;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
